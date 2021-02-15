b204fea1ff468f1fa70be3139c90f8b46cca17d8
cdamyx <chris@amyx.engineering>
2021-02-14 21:54:50 -0600

    finished weather app

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
 	
-	const url = "https://api.openweathermap.org/data/2.5/weather?q=pratt, ks, us&appid=fe79cf42e54a57f1d693d3509ee6dcb6&units=imperial";
+	res.sendFile(__dirname + "/index.html");
+
+})
+
+app.post("/", function(req, res){
+	// console.log(req.body.cityName);
+	
+	const query = req.body.cityName;
+	const apiKey = "fe79cf42e54a57f1d693d3509ee6dcb6";
+	const unit = "imperial";
+	const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
 	https.get(url, function(response){
 		console.log(response.statusCode);
@@ -17,19 +30,13 @@ app.get("/", function(req, res){
 			const icon = weatherData.weather[0].icon;
 			const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 			console.log(temp, desc);
-			res.write("<p>The weather is " + desc + ".</p>")
-			res.write("<h1>The temperature in Pratt is " + temp + " degrees Fahrenheit.</h1>")
+			res.write("<h1>Current weather: " + desc + ".</h1>")
+			res.write("<h2>The temperature in " + query + " is " + temp + " degrees Fahrenheit.</h2>")
 			res.write("<img src=" + imgUrl + ">")
 			res.send();
 		})
 	})
-
 })
-
-
-
-
-
 
 
 
